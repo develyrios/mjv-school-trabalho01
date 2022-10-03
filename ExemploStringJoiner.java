62ff0b4c0b819f45a62e512bd272a348168ad1e1
Bev Feitosa <bevely1407@gmail.com>
2022-10-03 16:06:34 -0300

    exemplo StringJoiner

--- ExemploStringJoiner.java
+++ ExemploStringJoiner.java
@@ -4,11 +4,11 @@ public class ExemploStringJoiner {
 
     //StringJoiner é usado para construir uma sequência de caracteres separados por um
     //delimitador (delimiter) e que podem vir acompanhada de um prefixo (prefix) ou de
-    //um sufixo (sufix)
+    //um sufixo (sufix). A classe está localizada no pacote java.util desde a versão JDK1.0.
 
-    //A classe está localizada no pacote java.util desde a versão JDK1.0
+    //Ela também pode ser feita através do método String.join()
 
-    //A instanciação pode ser feita da seguinte forma:
+    //Pode-se instanciar a classe da seguinte forma:
     StringJoiner stringJoinerExemplo = new StringJoiner(" delimitador ","prefixo "," sufixo");
 
     void run () {
@@ -28,8 +28,8 @@ public class ExemploStringJoiner {
         System.out.println(stringJoinerExemplo);
 
         //Opa! Apesar de termos declarado um delimitador, um prefixo e um sufixo, não adicionamos nada a essa string,
-        //por isso foi printada a mensagem que a String está vazia. É importante ter em mente que delimitador,
-        //prefixo e sufixo NÃO são considerados na hora de manusear ou alterar a String.
+        //por isso foi printada a mensagem que a String está vazia. É importante ter em mente que prefixo e sufixo NÃO
+        //são considerados na hora de manusear ou alterar a String.
     }
 
     private void exemplosAdd() {
@@ -37,7 +37,7 @@ public class ExemploStringJoiner {
         //Vamos entender melhor como funciona esse método e como ocorre o posicionamento do prefixo, do delimitador,
         //do sufixo e de novos caracteres dentro da String.
 
-        stringJoinerExemplo.add("caracteres 1").add("caracteres 2").add("caracteres 3");
+        stringJoinerExemplo.add("palavra-1").add("palavra-2").add("palavra-3");
         System.out.println(stringJoinerExemplo);
 
         //Uma vez que o prefixo, o delimitador e o sufixo não mudam mesmo com a adição de novos caracteres,
@@ -49,18 +49,23 @@ public class ExemploStringJoiner {
         listaDeCompras.add("leite");
         listaDeCompras.add("manteiga");
         listaDeCompras.add("mussarela");
-        listaDeCompras.add("extrato de tomate");
-        listaDeCompras.add("peito de frango");
+        System.out.println(listaDeCompras);
+
+        //Também podemos atribuir uma string a um argumento e depois adicioná-lo a outra string, vejamos:
+        String cafe = "café";
+        listaDeCompras.add(cafe);
         System.out.println(listaDeCompras);
     }
 
     private void exemplosMerge() {
         //O método .merge adiciona uma String a outra String, vejamos só:
 
+        //Intanciando e atribuindo caracteres à stringMerge
         StringJoiner stringMerge = new StringJoiner("");
-        stringMerge.add("caracteres merge 1");
+        stringMerge.add("palavra-merge-1");
         System.out.println(stringMerge);
 
+        //Mergeando stringMerge na stringJoinerExemplo
         stringJoinerExemplo.merge(stringMerge);
         System.out.println(stringJoinerExemplo);
 
@@ -68,15 +73,22 @@ public class ExemploStringJoiner {
         //como o método .add faz. Mas e se nossa string a ser mergeada tivesse prefixo, delimitador e sufixo?
 
         StringJoiner stringMergeCompleta = new StringJoiner(" delimitador-merge ", "prefixo-merge ", " sufixo-merge");
-        stringMergeCompleta.add("caracteres merge 2").add("caracteres merge 3").add("caracteres merge 4");
+        stringMergeCompleta.add("palavra-merge-2").add("palavra-merge-3").add("palavra-merge-4");
         System.out.println(stringMergeCompleta);
 
         stringJoinerExemplo.merge(stringMergeCompleta);
         System.out.println(stringJoinerExemplo);
 
         //Interessante! O prefixo e sufixo são deixados de fora, entretanto, a String nova é adicionada com
-        //o seu delimitador. Vamos aplicar esses conhecimentos a um novo exemplo:
+        //o seu delimitador. Vamos aplicar TODOS esses conhecimentos a um novo exemplo:
 
-        //exemplo Merge
+        StringJoiner listaAmo = new StringJoiner(", ","Eu amo "," demais");
+        listaAmo.add("música").add("pizza").add("bolo");
+
+        StringJoiner listaOdeio = new StringJoiner(" / ","Eu odeio ","muito!!!");
+        listaOdeio.add("jiló").add("barulho").add("insolação");
+
+        listaAmo.merge(listaOdeio);
+        System.out.println(listaAmo);
     }
 }
