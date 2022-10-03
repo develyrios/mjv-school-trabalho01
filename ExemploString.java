ade53440612d30f6c531e38aa4b92cfd5bcbb8a5
Isabela Telles <83988818+liratisa@users.noreply.github.com>
2022-10-03 07:46:58 -0300

    atualizando o arquivo com o método isEmpty()

--- ExemploString.java
+++ ExemploString.java
@@ -1,5 +1,15 @@
 public class ExemploString {
-    int void contarCaracteres(String texto) {
-        return texto.length();
-    }
+    void contarCaracteres() {
+        
+		// o método length() retorna o comprimento do texto em uma String		
+		String email = "email@teste.com";
+		int tamanho = email.length();
+		System.out.println(tamanho);
+		
+        // o método isEmpty() verifica se a string está vazia e retorna true se estiver e false, se não
+		String vazio = "";
+		boolean estaVazio = vazio.isEmpty();
+		System.out.println(estaVazio);
+		
+	}
 }
